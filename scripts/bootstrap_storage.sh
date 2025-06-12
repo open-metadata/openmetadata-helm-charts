2bb5860738232d663f31bf19958889cd29f55875
aniljith74 <aniljith.sathyanathan@tsworks.io>
2025-06-12 13:48:34 +0530

    docker updates

--- scripts/bootstrap_storage.sh
+++ scripts/bootstrap_storage.sh
@@ -3,8 +3,6 @@ set -e
 
 echo "Running OpenMetadata bootstrap..."
 
-# Run DB migrations, seed metadata, setup roles and policies
-/openmetadata/scripts/upgrade_schema.sh
-/openmetadata/scripts/bootstrap_data.sh
+/openmetadata/bootstrap/openmetadata-ops.sh bootstrap
 
 echo "Bootstrap complete!"
